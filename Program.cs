8d00e2769e69210ab56eeb58f270326fd4bac45b
ceboykin2 <ceboykin2@crimson.ua.edu>
2024-09-13 15:40:48 -0500

    Lab 3

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//main
+string enjoymentLevel = GetEnjoymentLevel();
+string stadium = GetStadiumRecommendation(enjoymentLevel);
+string game = GetGameRecommendation(stadium);
+DisplayStadiumDetails(stadium, game);
+//end main
+
+static string GetEnjoymentLevel(){
+    Console.Clear();
+    System.Console.WriteLine("What is your preferred level of enjoyment?");
+    return Console.ReadLine().ToLower();
+}
+
+
+static string GetStadiumRecommendation(string enjoymentLevel){
+    if(enjoymentLevel == "boring"){
+        return "Neyland Stadium";
+    }   
+    else if(enjoymentLevel == "average"){
+        return "Jordan-Hare Stadium";
+    }
+    else if(enjoymentLevel == "fun"){
+        return "Tiger Stadium";
+    }
+    else if(enjoymentLevel == "epic"){
+        return "Saban Field at Bryant-Denny Stadium";
+    }
+    else{
+        return "Invalid input";
+    }
+}
+
+static string GetGameRecommendation(string stadium){
+    if(stadium == "Neyland Stadium"){
+        return "vs Kent State";
+    }
+    else if(stadium == "Jordan-Hare Stadium"){
+        return "vs Kentucky";
+    }
+    else if(stadium == "Tiger Stadium"){
+        return "vs Alabama";
+    }
+    else{
+        return "vs Auburn";
+    }
+}
+
+static void DisplayStadiumDetails(string stadium, string game){
+    if(stadium != "Invalid input"){
+        System.Console.WriteLine($"The best game for you to attend is at {stadium} {game}!");
+    }
+    else{
+        System.Console.WriteLine(stadium);
+    }
+}
